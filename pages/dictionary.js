5556ed987b4058b6179aba1f00aff768e6403057
Huzthepro <hzyfakcay@gmail.com>
2023-01-10 23:37:33 +0100

    Fixed: minor problems

--- pages/dictionary.js
+++ pages/dictionary.js
@@ -1,4 +1,8 @@
 import {
+  LOGO_SECTION,
+  LOGO,
+  LOGO_SRC,
+  LOGO_ALT,
   INPUT_SECTION,
   INPUT_CONTAINER,
   INPUT_ELEMENT,
@@ -21,8 +25,18 @@ export const initDictionaryPage = () => {
   const body = document.body;
   body.innerHTML = '';
 
-  // ------------------ ↓ Input Section ↓ ------------------
+  //Logo div
+  const logoSection = document.createElement('div');
+  logoSection.id = LOGO_SECTION;
+  body.appendChild(logoSection);
+  //Logo
+  const logo = document.createElement('img');
+  logo.id = LOGO;
+  logo.src = LOGO_SRC;
+  logo.alt = LOGO_ALT;
+  logoSection.appendChild(logo);
 
+  // ------------------------------------------ ↓ Input Section ↓ -----------------------------------------
   //Input section
   const inputSection = document.createElement('div');
   inputSection.id = INPUT_SECTION;
@@ -43,84 +57,98 @@ export const initDictionaryPage = () => {
   searchButton.innerHTML = SEARCH_BUTTON_TEXT;
   inputContainer.appendChild(searchButton);
 
-  searchButton.addEventListener('click', search); // ← ← Click on Search
+  searchButton.addEventListener('click', search); // ← ← Click to Search
+  // ------------------------------------------ ↑ Input Section ↑ -----------------------------------------
 
-  // ↓ Search Function ↓
-  async function search() {
-    const inputValue = inputElement.value;
-    inputElement.value = ''; // ← ← Previous result should have been cleaned
-
-    try {
-      const fetchedData = await fetchUrl(inputValue);
-      console.log(fetchedData);
-      resultSectionFiller(fetchedData);
-    } catch (error) {
-      return error;
-    }
-  }
-
+  // ↓ With -Enter key- it should also search ↓
   inputElement.addEventListener('keypress', function (event) {
     if (event.key === 'Enter') {
       event.preventDefault();
       document.getElementById(SEARCH_BUTTON_ID).click();
     }
   });
+  // ↓ Search Function ↓
+  async function search() {
+    const inputValue = inputElement.value;
+    inputElement.value = ''; // ← ← Previous result should have been cleaned
+    try {
+      const fetchedData = await fetchUrl(inputValue);
+      resultSectionFiller(fetchedData); // ← ← With fetched data we will build result section
+    } catch (error) {
+      return error;
+    }
+  }
 
-  //Result section
+  // ------------------------------------------ ↓ Result Section ↓ ------------------------------------------
+  // Result section
   const resultSection = document.createElement('div');
   resultSection.id = RESULT_SECTION;
   body.appendChild(resultSection);
 
+  // ↓ ↓This function responsible for building result section ↓ ↓
   function resultSectionFiller(fetchedData) {
-    //clearing -resultSection- before use
-    resultSection.innerHTML = '';
+    resultSection.innerHTML = ''; // ← ← Clearing -resultSection- before use
 
+    // Result container
     const resultContainer = document.createElement('div');
     resultContainer.id = RESULT_CONTAINER;
     resultSection.appendChild(resultContainer);
 
+    // The word that user searched
     const searchedWord = document.createElement('h1');
     searchedWord.id = SEARCHED_WORD;
     try {
+      // ← ← For case if it can't find the word
       searchedWord.textContent = fetchedData[0].word;
     } catch (error) {
       searchedWord.textContent = `Couldn't find the word`;
     }
     resultContainer.appendChild(searchedWord);
 
+    // Pronunciation Block
     const pronunciationBlock = document.createElement('div');
     pronunciationBlock.id = PRONUNCIATION_BLOCK;
     resultContainer.appendChild(pronunciationBlock);
-
+    // Pronunciation
     const pronunciation = document.createElement('h4');
     pronunciation.id = PRONUNCIATION;
     pronunciation.textContent = fetchedData[0].phonetic || '- -';
     pronunciationBlock.appendChild(pronunciation);
-
+    // Pronunciation Voice
     const audioButton = document.createElement('div');
     audioButton.id = AUDIO_BUTTON_ID;
     audioButton.innerHTML = AUDIO_BUTTON_TEXT;
     pronunciationBlock.appendChild(audioButton);
-    audioButton.addEventListener('click', playAudio);
+    audioButton.addEventListener('click', playAudio); // ← ← Click to Listen
 
+    const audio = document.createElement('audio');
+    // ↓ ↓ Sometimes sound in other -phonetics- ↓ ↓
+    fetchedData[0].phonetics.forEach((element) => {
+      if (element.audio != '') {
+        audio.src = element.audio;
+      }
+    });
+    // ↓ ↓ If can't find, remove the button
+    if (audio.src === '') {
+      audioButton.remove();
+    }
+
+    // ↓ ↓ This function responsible for listening pronunciation ↓ ↓
     function playAudio() {
-      const audio = document.createElement('audio');
-      fetchedData[0].phonetics.forEach((element) => {
-        if (element.audio != '') {
-          audio.src = element.audio;
-        }
-      });
       audio.play();
     }
 
+    // Meaning
     const meaning = document.createElement('h2');
     meaning.id = MEANING;
     meaning.textContent =
       fetchedData[0].meanings[0].definitions[0].definition || '- -';
     resultContainer.appendChild(meaning);
 
+    // Example
     const example = document.createElement('h3');
     example.id = EXAMPLE;
+    // ↓ ↓ This function responsible for finding last example ↓ ↓
     fetchedData[0].meanings.forEach((element) => {
       element.definitions.forEach((element) => {
         if (element.example) {
@@ -129,18 +157,14 @@ export const initDictionaryPage = () => {
       });
     });
     resultContainer.appendChild(example);
+    // ------------------------------------------ ↑ Result Section ↑ -----------------------------------------
   }
 };
 
+// ↓ ↓ This function responsible for fetching ↓ ↓
 function fetchUrl(inputValue) {
-  function handleErrors(response) {
-    if (!response.ok) {
-      throw Error(response.statusText);
-    }
-    return response;
-  }
   return fetch(`${DICTIONARY_URL}${inputValue}`)
-    .then(handleErrors)
+    .then(handleErrors) // ← ← For case fetch return wrong value as -resoled-
     .then((response) => response.json())
     .then((data) => {
       return data;
@@ -148,4 +172,11 @@ function fetchUrl(inputValue) {
     .catch((error) => {
       return error;
     });
+  // ↓ ↓ Fetch need extra touch to check for errors ↓ ↓
+  function handleErrors(response) {
+    if (!response.ok) {
+      throw Error(response.statusText);
+    }
+    return response;
+  }
 }
