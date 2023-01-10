b776c6a4a8b8a3635f0dc394651809079a300966
Huzthepro <hzyfakcay@gmail.com>
2023-01-10 23:45:00 +0100

    fixed - logo

--- pages/dictionary.js
+++ pages/dictionary.js
@@ -32,7 +32,7 @@ export const initDictionaryPage = () => {
   //Logo
   const logo = document.createElement('img');
   logo.id = LOGO;
-  logo.src = LOGO_SRC;
+  logo.src = '../images/dict-logo.png';
   logo.alt = LOGO_ALT;
   logoSection.appendChild(logo);
 
